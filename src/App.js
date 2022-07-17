386c5003fe9afbe71afddb5a189c74a507d081fb
ridhoabdurrahman <ridhoabd007@gmail.com>
2022-07-17 19:24:47 +0700

    Clean Code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
+import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import 'react-native-gesture-handler';
-import {SafeAreaView, StatusBar, Text} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {SplashScreen, SignIn} from './pages';
 import Router from './router';
 
 const App = () => {
